248089124aaac47194a01ce39c73fc665ae12a02
XogosAquiDEV <olimial03@gmail.com>
2025-09-01 14:37:07 +0200

    añadiendo menus

---
+++ Scripts/menu_inicial.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://Escenas/selección_de_niveles.tscn")#Va al menu de selección de niveles
+
+func _on_exit_pressed():
+	get_tree().quit() #cierra el juego
