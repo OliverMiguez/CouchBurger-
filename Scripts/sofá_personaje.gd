0fb1bf9e5f3138993827a9a0e282683edb9702bb
XogosAquiDEV <olimial03@gmail.com>
2025-09-02 14:47:54 +0200

    Refactor Player

--- Scripts/sofá_personaje.gd
+++ Scripts/sofá_personaje.gd
@@ -1,23 +1,21 @@
-##Clase del jugador que manejamos
-#Debug ? 
 extends CharacterBody2D
 class_name Personaje
 
-#velocidad de movimiento lateral
-@export var speed: float = 400.0
+@export var speed: float = 400.0 # velocidad de movimiento lateral
 
 func _physics_process(delta):
-	var direction = 0 #para calcular los movimiento laterales
-	#Scripts de movimientos
+	var direction = 0 # Propiedad para calcular los movimiento laterales
+	# Scripts de movimientos
 	if Input.is_action_pressed("ui_right"):
 		direction += 1
 	if Input.is_action_pressed("ui_left"):
 		direction -= 1
 
-	velocity.x = direction * speed
+	velocity.x = direction * speed # Velocidad de movimiento del player 
 	velocity.y = 0  # si no hay gravedad
 
-	move_and_slide()
-	
+	move_and_slide() # Permite el movimiento del player
+
+## Función para llamar al player en otras escenas
 func player():
 	pass
